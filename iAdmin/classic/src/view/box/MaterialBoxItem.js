3dfb50ebc4328a3ba8a80ad607ad66ff226977f3
SamuelOliveira <samuca95@hotmail.com>
2016-06-18 19:52:41 -0300

    Refresh

--- iAdmin/classic/src/view/box/MaterialBoxItem.js
+++ iAdmin/classic/src/view/box/MaterialBoxItem.js
@@ -37,6 +37,7 @@ Ext.define( 'iAdmin.view.box.MaterialBoxItem', {
     buildItems: function () {
         var me = this;
 
+        
         Ext.create('iAdmin.store.box.MaterialBoxItem');
 
         me.columns = [
