184f1030f91e1458d898a83ef3f8b483012c4a1c
SamuelOliveira <samuel.dasilva@iatec.com>
2016-07-11 10:53:48 -0300

    Fix Material SelectLike

---
+++ iSterilization/business/Quick/ServiceRegistration.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace iSterilization\Quick;
+
+use Smart\Data\Proxy;
+use Smart\Setup\Start;
+use Smart\Utils\Report;
+use Smart\Utils\Session;
+
+class ServiceRegistration extends Report {
+
+    private $proxy;
+
+    public function preConstruct() {
+        $this->post = (object) self::decodeUTF8($_REQUEST);
+
+        $id = $this->post->id;
+
+        $sizeColumns = array(20,20,20,20,15,15);
+        $this->sizeColumns = $this->scaleCalc(array_sum($sizeColumns),intval($this->getInternalW()),$sizeColumns);
+        $this->squareWidth = intval($this->getInternalW() / 12);
+
+        $this->proxy = new Proxy(array(Start::getConnnect(), Start::getUserName(), Start::getPassWord()));
+
+        $sql = "
+            select
+                sr.resultvalue,
+                coalesce(sr.resultfield,ib.resultfield) as resultfield
+            from
+                serviceregistration sr
+                left join itembase ib on ( ib.id = sr.itembaseid )
+            where sr.id = :id
+              and coalesce(sr.resultfield,ib.resultfield) is not null";
+
+        $pdo = $this->proxy->prepare($sql);
+        $pdo->bindValue(":id", $id, \PDO::PARAM_INT);
+
+        $pdo->execute();
+        $this->rows = $pdo->fetchAll();
+
+        $this->setResultValue();
+    }
+
+    public function setResultValue () {
+
+        $rows = $this->rows;
+
+        if(count($rows) != 0) {
+            $resultvalue = $rows[0]['resultvalue'];
+            $resultfield = $rows[0]['resultfield'];
+
+            $i = 0;
+            $json = self::jsonToArray($resultvalue);
+            $base = self::jsonToArray($resultfield);
+
+            foreach ($base as $item) {
+                $list[$i]['id'] = $i+1;
+                $defaultValue = $item['defaultValue'];
+                $value = isset($json[$i]['value']) ? $json[$i]['value'] : '';
+
+                $defaultValue = strlen($value) != 0 ? $value : $defaultValue;
+
+                $list[$i]['datavalue'] = $defaultValue;
+                $list[$i]['fieldname'] = $item['name'];
+                $list[$i]['fieldtext'] = $item['displayName'];
+                $list[$i]['reference'] = $item["referenceValue"];
+                $list[$i]['formfield'] = self::arrayToJson($item);
+                $list[$i]['showorder'] = str_pad($item['showOrder'],2,'0',STR_PAD_LEFT);
+                $i++;
+            }
+
+            $rows = self::sorterArray($list,'showorder');
+        }
+
+        $this->rows = $rows;
+
+        return $rows;
+    }
+
+    public function posConstruct() {
+        $this->AliasNbPages();
+        $this->AddFont('LucidaSans-Typewriter','','LTYPE.php');
+        $this->setAllMarginPage(12);
+        $this->AddPage();
+        $this->Detail();
+        $this->Output("ServiceRegistration.pdf", "I");
+    }
+
+    public function setAllMarginPage($margin) {
+        $this->SetMargins($margin,$margin + 2);
+        $this->SetAutoPageBreak(false,$margin);
+    }
+
+    public function Header() {
+        $this->squareWidth = intval($this->getInternalW() / 6);
+
+//        $id = $this->rows[0]['id'];
+//        $cmeareasname = $this->rows[0]['cmeareasname'];
+//        $movimentdate = $this->rows[0]['movimentdate'];
+//        $movimenttypedescription = $this->rows[0]['movimenttypedescription'];
+//        $documenttypedescription = $this->rows[0]['documenttypedescription'];
+//        $movimentstatusdescription = $this->rows[0]['movimentstatusdescription'];
+//
+//        $date = new \DateTime($movimentdate);
+//        $id = str_pad($id, 6, '0', STR_PAD_LEFT);
+//
+//        $this->configStyleHeader(14);
+//        $this->Cell($this->getInternalW(),6, "Movimento de $movimenttypedescription N# $id",0,1,'C',false);
+//        $this->configStyleHeader(10);
+//        $this->Cell($this->getInternalW(),6, 'Data: '. $date->format('d/m/Y'),0,1,'C',false);
+//
+//        $this->SetLineWidth(.2);
+//        $this->Cell($this->getInternalW(),3, '','B',1,'C');
+//        $this->Ln(4);
+//
+//        $this->SetFont('Arial', '', 10);
+//        $this->Cell($this->squareWidth,5,'Documento:',0,0,'L',0);
+//        $this->configStyleHeader(10);
+//        $this->Cell($this->squareWidth*4,5,"$documenttypedescription - $movimentstatusdescription",0,1,'L',0);
+//        $this->SetFont('Arial', '', 10);
+//        $this->Cell($this->squareWidth,5,'CMEArea:',0,0,'L',0);
+//        $this->configStyleHeader(10);
+//        $this->Cell($this->squareWidth*4,5,$cmeareasname,0,1,'L',0);
+//        $this->Ln(1);
+    }
+
+    public function getHeaderColumns() {
+        $sw = $this->squareWidth;
+
+        $this->SetFont('Arial', '', 9);
+        $this->SetFillColor(245, 242, 198);
+
+        $this->Cell($sw * 1.0,7,'Campo','B',0,'L',1);
+        $this->Cell($sw * 2.0,7,'Resultado','B',0,'L',1);
+        $this->Cell($sw * 3.0,7,'Referencia','B',1,'L',1);
+    }
+
+    public function Detail() {
+        $sw = $this->squareWidth;
+
+        $lineColor = 1;
+
+        $this->getHeaderColumns();
+
+        while(list(, $item) = each($this->rows)) {
+            extract($item);
+
+            //Control line color
+            $lineColor = ($lineColor == 0) ? 1 : 0;
+
+            //Config Style Details
+            $this->configStyleDetail();
+            $this->SetFont('LucidaSans-Typewriter', '', 8);
+
+            //Print Datas
+            $this->Cell($sw * 1.0,5,"$showorder. $fieldtext",0,0,'L',$lineColor);
+            $this->Cell($sw * 2.0,5,$datavalue,0,0,'L',$lineColor);
+            $this->Cell($sw * 3.0,5,$reference,0,1,'L',$lineColor);
+        }
+
+        $this->SetLineWidth(.2);
+        $this->Cell($this->getInternalW(),3, '','T',1,'C');
+
+        $this->Ln(20);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell($sw * 2.0,4, utf8_decode("Lançado por"),'T',0,'C');
+        $this->Cell($sw * 2.0,3, '',0,0,'C');
+        $this->Cell($sw * 2.0,4, utf8_decode("Encerrado por"),'T',1,'C');
+    }
+
+    public function Footer() {
+        $this->loadFooter($this->getInternalW(),false);
+    }
+
+}
