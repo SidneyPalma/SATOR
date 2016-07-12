a094d289e00f816fdbef1c60e35d3c93b12ba5fe
SamuelOliveira <samuel.dasilva@iatec.com>
2016-07-11 21:24:46 -0300

    processing.FlowProcessing

---
+++ iSterilization/app/model/processing/FlowProcessing.js
@@ -0,0 +1,66 @@
+//@charset UTF-8
+Ext.define( 'iSterilization.model.processing.FlowProcessing', {
+    extend: 'Ext.data.Model',
+
+    requires: [
+        'Smart.util.Resource'
+    ],
+
+    fields: [
+        {
+            name: 'id',
+            type: 'int'
+        }, {
+            name: 'itembaseid',
+            type: 'int'
+        }, {
+            name: 'itembasename',
+            type: 'auto'
+        }, {
+            name: 'description',
+            type: 'auto'
+        }, {
+            name: 'barcode',
+            type: 'auto'
+        }, {
+            name: 'manufacturerid',
+            type: 'int'
+        }, {
+            name: 'manufacturername',
+            type: 'auto'
+        }, {
+            name: 'itembasetype',
+            type: 'auto'
+        }, {
+            name: 'itembasetypedescription',
+            type: 'auto'
+        }, {
+            name: 'registrationanvisa',
+            type: 'auto'
+        }, {
+            name: 'cmeareasid',
+            type: 'int'
+        }, {
+            name: 'cmeareasname',
+            type: 'auto'
+        }, {
+            name: 'filedata',
+            type: 'auto',
+            convert: function (value, record) {
+                return (value) ? value : Smart.Rss.getFileImage('smart');
+            }
+        }, {
+            name: 'fileinfo',
+            type: 'auto'
+        }, {
+            name: 'filetype',
+            type: 'auto',
+            convert: function (value, record) {
+                var info = record.get('fileinfo'),
+                    type = (info && info.length !== 0) ? Ext.decode(info) : null;
+                return (type) ? Ext.String.format('data:{0};base64,{1}', type.fileType, record.get('filedata')) : record.get('filedata');
+            }
+        }
+    ]
+
+});
--- iSterilization/classic/src/view/service/ServiceItemBaseSearch.js
+++ iSterilization/classic/src/view/service/ServiceItemBaseSearch.js
@@ -22,9 +22,9 @@ Ext.define( 'iSterilization.view.service.ServiceItemBaseSearch', {
                 '<div style="float: left; height: 80px; width: 120px; margin-right: 10px;">',
                     '<img src="{filetype}" style="height: 100%; width: 100%;">',
                 '</div>',
-                '<div style="font-size: 22px; line-height: 40px;">{itembasename}</div>',
+                '<div style="font-size: 22px; line-height: 40px; background-color: rgba(231, 242, 225, 1); color: black;">{itembasename}</div>',
                 '<div style="font-size: 16px; line-height: 20px; background-color: rgba(250, 241, 210, .4);">{manufacturername}</div>',
-                '<div style="font-size: 14px; line-height: 20px; background-color: rgba(250, 241, 210, .4);">Código de Barras: {barcode}</div>',
+                '<div style="font-size: 14px; line-height: 20px; background-color: rgba(250, 241, 210, .4);">Código de Barras: <a style="color: red;">{barcode}</a></div>',
             '</div>',
         '</tpl>'
     ]
