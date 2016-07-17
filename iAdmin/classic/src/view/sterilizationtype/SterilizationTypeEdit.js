3a9c4d2eacbc4d7dae868b1d53d04c6790b8a5ca
SamuelOliveira <samuel.dasilva@iatec.com>
2016-07-16 23:35:36 -0300

    Refactoring

---
+++ iAdmin/classic/src/view/sterilizationtype/SterilizationTypeEdit.js
@@ -0,0 +1,85 @@
+//@charset UTF-8
+Ext.define( 'iAdmin.view.sterilizationtype.SterilizationTypeEdit', {
+    extend: 'Ext.window.Window',
+
+    xtype: 'sterilizationtypeedit',
+
+    requires: [
+        'Ext.form.Panel',
+        'Ext.window.Window',
+        'Smart.form.field.ComboEnum',
+        'iAdmin.store.sterilizationtype.*'
+    ],
+
+    constrain: true,
+
+    width: 450,
+    resizable: false,
+    showAnimate: true,
+    layout: 'fit',
+    controller: 'sterilizationtype',
+    cls: 'panel-frame',
+    iconCls: "fa fa-pencil",
+
+    title: 'Editar Fluxo',
+
+    initComponent: function () {
+        var me = this;
+        me.buildItems();
+        me.callParent();
+    },
+
+    buildItems: function () {
+        var me = this;
+
+        me.items = [
+            {
+                xtype: 'form',
+                bodyPadding: 10,
+                layout: 'anchor',
+                defaultType: 'textfield',
+                defaults: {
+                    anchor: '100%',
+                    allowBlank: false
+                },
+                items: [
+                    {
+                        allowBlank: true,
+                        xtype: 'hiddenfield',
+                        name: 'id'
+                    }, {
+                    //     allowBlank: true,
+                    //     xtype: 'hiddenfield',
+                    //     name: 'graphpaper'
+                    // }, {
+                        useReadColor: true,
+                        name: 'name',
+                        fieldLabel: 'Nome'
+                    }, {
+                        xtype: 'textareafield',
+                        name: 'description',
+                        fieldLabel: 'Descrição'
+                    }
+                ]
+            }
+        ]
+    },
+
+    buttonAlign: 'center',
+
+    buttons: [
+        {
+            iconCls: "fa fa-upload",
+            text: 'Salvar',
+            showSmartTheme: 'red',
+            handler: 'updateView'
+        }, {
+            text: 'Fechar',
+            showSmartTheme: 'red',
+            handler: function (btn) {
+                btn.up('window').close();
+            }
+        }
+    ]
+
+});
