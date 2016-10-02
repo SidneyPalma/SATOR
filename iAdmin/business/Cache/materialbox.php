f21edf182c930c63ce2bf779688a84a3236cb8db
SamuelOliveira <samuel.dasilva@iatec.com>
2016-10-01 23:11:20 -0300

    14) Cadastro de Kits - Clone do master, o detail ficar vazio;

--- iAdmin/business/Cache/materialbox.php
+++ iAdmin/business/Cache/materialbox.php
@@ -64,4 +64,94 @@ class materialbox extends \Smart\Data\Cache {
         return self::getResultToJson();
     }
 
+    public function insertCopy(array $data) {
+        $id = $data['id'];
+        $username = $this->session->username;
+        $proxy = $this->getStore()->getProxy();
+
+
+        $sql = "
+            SET XACT_ABORT ON
+            SET NOCOUNT ON
+            SET ANSI_NULLS ON
+            SET ANSI_WARNINGS ON
+            
+            declare
+                @nw int,
+                @id int = {$id},
+                @barcode varchar(20), 
+                @error_code int = 0, 
+                @error_text nvarchar(200),
+                @username varchar(80) = '{$username}';
+            
+            BEGIN TRY
+            
+                BEGIN TRAN setCloneItem;
+            
+                    insert into 
+                        materialbox ( name, barcode, restriction, itemsize, statusbox, packingid, requirepatient )
+                    select
+                        '(clone) ' + mb.name, 
+                        '(clone)' as barcode,
+                        mb.restriction, 
+                        mb.itemsize, 
+                        mb.statusbox, 
+                        mb.packingid, 
+                        mb.requirepatient
+                    from
+                        materialbox mb
+                    where mb.id = @id;
+            
+                    set @nw = ( select @@identity );            
+            
+                    set @barcode = 'K' + dbo.getLeftPad(7,'0',@nw);
+            
+                    update materialbox set barcode = @barcode where id= @nw;
+            
+                    insert into materialboxtarge ( materialboxid, targecolorid, targeorderby )
+                    select
+                        @nw as materialboxid, mbi.targecolorid, mbi.targeorderby
+                    from
+                        materialboxtarge mbi
+                    where mbi.materialboxid = @id;            
+            
+                COMMIT TRAN setCloneItem;
+            
+                set @error_code = 0;
+                set @error_text = 'Atualizacoes realizadas com sucesso!';
+            
+            END TRY
+            
+            BEGIN CATCH
+                ROLLBACK TRAN setCloneItem;
+                set @error_code = error_number();
+                set @error_text = ' # ' + error_message() + ', ' + cast(error_line() as varchar);
+            END CATCH
+            
+            select @nw as id, @error_code as error_code, @error_text as error_text;";
+
+        try {
+            $rows = $proxy->query($sql)->fetchAll();
+
+            $message = $rows[0]['error_text'];
+            $success = intval($rows[0]['error_code']) == 0;
+
+            if($success == true) {
+                $data['query'] = $rows[0]['id'];
+                $this->selectCode($data);
+                return $this->selectCode($data);
+            }
+
+            self::_setRows($rows);
+            self::_setText($message);
+            self::_setSuccess($success);
+
+        } catch ( \PDOException $e ) {
+            self::_setSuccess(false);
+            self::_setText($e->getMessage());
+        }
+
+        return self::getResultToJson();
+    }
+
 }
