ed7892d2f9cf89f7d42e684fbc020f9368ca36fe
SamuelOliveira <samuel.dasilva@iatec.com>
2016-11-23 20:55:29 -0200

    4) Nas telas de movimento ao se criar o movimento já habilitar o campo de leitura do processo;

--- iSterilization/classic/src/view/flowprocessing/FlowProcessingController.js
+++ iSterilization/classic/src/view/flowprocessing/FlowProcessingController.js
@@ -279,7 +279,7 @@ Ext.define( 'iSterilization.view.flowprocessing.FlowProcessingController', {
                                             Smart.Msg.showToast(resultSet.getMessage(), 'error');
                                             return false;
                                         }
-
+console.info(view.master);
                                         var url = 'business/Calls/Quick/DispensingReport.php?id={0}';
                                         window.open(Ext.String.format(url,data.get('id')));
 
